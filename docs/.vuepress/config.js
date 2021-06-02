9d5dcd5d30bc7441807467bad1b8f614a6f79287
icekylin <wminid@yeah.net>
2021-06-02 11:38:37 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -139,6 +139,21 @@ module.exports = {
     ],
     [
       "script",
+      {},
+      `(function () {
+  var bp = document.createElement("script");
+  var curProtocol = window.location.protocol.split(":")[0];
+  if (curProtocol === "https") {
+    bp.src = "https://zz.bdstatic.com/linksubmit/push.js";
+  } else {
+    bp.src = "http://push.zhanzhang.baidu.com/push.js";
+  }
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(bp, s);
+})();`,
+    ],
+    [
+      "script",
       {
         async: "",
         src: "https://www.googletagmanager.com/gtag/js?id=G-MQRPJQK9SC",
