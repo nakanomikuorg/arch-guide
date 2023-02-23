c5cb747ffbb60c64372037e8ef6e4e93a64593e9
ice-kylin <56141162+ice-kylin@users.noreply.github.com>
2023-02-23 21:20:16 +0000

    Deploying to gh-pages from @ NakanoMikuOrg/arch-guide@4e3313abe7dff2c62aa5e4a91b8821718b1c1364 🚀

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e7059486ad3b16e21ea7058836c51b9b";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
